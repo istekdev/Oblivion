7e631a7efee9e8cc5f9f8883a6c971522ab8e425
ISTEK <tamdacoder@gmail.com>
2025-04-06 14:53:48 -0500

    Create header.js

---
+++ public/scripts/header.js
@@ -0,0 +1,13 @@
+document.write(`
+<header>
+ <div id="header-left">
+   <a href="/" class="header-title"><img class="header-logo" src="/images/logo/oblivion-word-invert-transparent.png" alt="Oblivion"></a>
+ </div>
+   <div class="header-right">
+   <div class="time-box">
+   <p class="time" id="time"></p>
+   </div>
+   </div>
+ </div>
+</header>
+`);
