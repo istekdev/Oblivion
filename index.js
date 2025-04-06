4afc61357d767db716d24aaa01b5df888f22fe80
ISTEK <tamdacoder@gmail.com>
2025-04-06 14:50:00 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,69 @@
+import wisp from "wisp-server-node"
+import { createBareServer } from "@tomphttp/bare-server-node"
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet"
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport"
+import { bareModulePath } from "@mercuryworkshop/bare-as-module3"
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node"
+import express from "express";
+import { createServer } from "node:http";
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const bare = createBareServer("/bare/")
+const __dirname = join(fileURLToPath(import.meta.url), "..");
+const app = express();
+const publicPath = "public";
+
+app.use(express.static(publicPath));
+app.use("/uv/", express.static(uvPath));
+app.use("/epoxy/", express.static(epoxyPath));
+app.use("/baremux/", express.static(baremuxPath));
+app.use("/baremod/", express.static(bareModulePath));
+
+app.use((req, res) => {
+    res.status(404);
+    res.sendFile(join(__dirname, publicPath, "404.html"));
+});
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeRequest(req, res);
+    } else {
+        app(req, res);
+    }
+});
+
+server.on("upgrade", (req, socket, head) => {
+    if (req.url.endsWith("/wisp/")) {
+        wisp.routeRequest(req, socket, head);
+    } else if (bare.shouldRoute(req)) {
+        bare.routeUpgrade(req, socket, head);
+    } else {
+        socket.end();
+    }
+});
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
+server.on("listening", () => {
+    const address = server.address();
+});
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+    console.log("SIGTERM signal received: closing HTTP server");
+    server.close();
+    bare.close();
+    process.exit(0);
+}
+
+console.log(`OBLIVION IS ON PORT 8080`);
+server.listen({
+    port,
+});
